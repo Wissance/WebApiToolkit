23203fa8f0f128d41d8051d01473f337a7ae3e10
Ushakov Michael <um.nix.user@gmail.com>
2024-01-21 23:09:26 +0500

    docs commentaries

--- Wissance.WebApiToolkit/Wissance.WebApiToolkit/Services/IResourceBasedCrudService.cs
+++ Wissance.WebApiToolkit/Wissance.WebApiToolkit/Services/IResourceBasedCrudService.cs
@@ -6,7 +6,8 @@ namespace Wissance.WebApiToolkit.Services
     /// <summary>
     ///    This is a Resource based CRUD Service interface to interact in a Resource-oriented way
     ///    via GRPC, could be used also for others Frameworks &amp;&amp; Protocols like SignalR &amp;&amp; WCF. This interface should be
-    ///    implemented in some real service classes. Examples will be here: https://github.com/Wissance/WeatherControl 
+    ///    implemented in some real service classes. Examples will be here: https://github.com/Wissance/WeatherControl . Contains methods
+    ///    for Create, Update and Delete TData item respectively.
     /// </summary>
     /// <typeparam name="TRes">TRes (Resource) means Representation of Persistent data in external system i.e. DTO</typeparam>
     /// <typeparam name="TData">Persistent item type, in terms of Web App it is a Table or some ORM Entity Class</typeparam>
--- Wissance.WebApiToolkit/Wissance.WebApiToolkit/Services/ResourceBasedDataManageableCrudService.cs
+++ Wissance.WebApiToolkit/Wissance.WebApiToolkit/Services/ResourceBasedDataManageableCrudService.cs
@@ -3,6 +3,14 @@ using Wissance.WebApiToolkit.Dto;
 
 namespace Wissance.WebApiToolkit.Services
 {
+    /// <summary>
+    ///     This is a full CRUD service implementation based on usage IModelManager as a service class for accessing
+    ///     persistent storage. This class has both Read operations (from ResourceBasedDataManageableReadOnlyService)
+    ///     and Create, Update and Delete operations
+    /// </summary>
+    /// <typeparam name="TRes">TRes (Resource) means Representation of Persistent data in external system i.e. DTO</typeparam>
+    /// <typeparam name="TData">Persistent item type, in terms of Web App it is a Table or some ORM Entity Class</typeparam>
+    /// <typeparam name="TId">Unique Identifier type (could be different for different apps i.e int/string/Guid)</typeparam>
     public class ResourceBasedDataManageableCrudService<TRes, TData, TId> : ResourceBasedDataManageableReadOnlyService<TRes, TData, TId>,
         IResourceBasedCrudService<TRes, TData, TId>
        where TRes: class
