f11cb4af8ca972defa884fcca6dc34f577793e82
Alex Mamzikov <av.mamzikov@gmail.com>
2024-05-07 18:59:57 +0500

    documentation is added

--- Wissance.WebApiToolkit/Wissance.WebApiToolkit/Controllers/BasicReadController.cs
+++ Wissance.WebApiToolkit/Wissance.WebApiToolkit/Controllers/BasicReadController.cs
@@ -14,10 +14,19 @@ namespace Wissance.WebApiToolkit.Controllers
         where TRes : class
         where TFilter : class
     {
+        /// <summary>
+        /// Returns a list of objects
+        /// </summary>
+        /// <param name="page">Page number</param>
+        /// <param name="size">Page size</param>
+        /// <param name="sort">Field name for sorting by it</param>
+        /// <param name="order">Sorting order: asc or desc</param>
+        /// <param name="additionalFilters"></param>
+        /// <returns></returns>
         [HttpGet]
         [Route("api/[controller]")]
         public virtual async Task<PagedDataDto<TRes>> ReadAsync([FromQuery] int? page, [FromQuery] int? size, [FromQuery] string sort,
-                                                                [FromQuery] string order, TFilter additionalFilters)
+                                                                [FromQuery] string order, [FromQuery] TFilter additionalFilters)
         {
             int pageNumber = GetPage(page);
             int pageSize = GetPageSize(size);
@@ -27,6 +36,11 @@ namespace Wissance.WebApiToolkit.Controllers
             return new PagedDataDto<TRes>(pageNumber, result.Data.Item2, PagingUtils.GetTotalPages(result.Data.Item2, pageSize), result.Data.Item1);
         }
 
+        /// <summary>
+        /// Returns one object by identifier
+        /// </summary>
+        /// <param name="id">Identifier of the object</param>
+        /// <returns></returns>
         [HttpGet]
         [Route("api/[controller]/{id}")]
         public async Task<TRes> ReadByIdAsync([FromRoute] TId id)
