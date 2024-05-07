fcac79e6e4e035eb292a03a1bcc44524e7f54dd9
Alex Mamzikov <av.mamzikov@gmail.com>
2024-05-07 18:19:32 +0500

    sort options is not nullable

--- Wissance.WebApiToolkit/Wissance.WebApiToolkit/Controllers/BasicReadController.cs
+++ Wissance.WebApiToolkit/Wissance.WebApiToolkit/Controllers/BasicReadController.cs
@@ -11,17 +11,17 @@ namespace Wissance.WebApiToolkit.Controllers
 {
 
     public abstract class BasicReadController<TRes, TData, TId, TFilter> : BasicPagedDataController
-        where TRes: class
-        where TFilter: class
+        where TRes : class
+        where TFilter : class
     {
         [HttpGet]
         [Route("api/[controller]")]
-        public virtual async Task<PagedDataDto<TRes>> ReadAsync([FromQuery] int? page, [FromQuery] int? size, [FromQuery] string sort, 
-                                                                [FromQuery] string order, TFilter additionalFilters = null)
+        public virtual async Task<PagedDataDto<TRes>> ReadAsync([FromQuery] int? page, [FromQuery] int? size, [FromQuery] string sort,
+                                                                [FromQuery] string order, TFilter additionalFilters)
         {
             int pageNumber = GetPage(page);
             int pageSize = GetPageSize(size);
-            SortOption sorting = !string.IsNullOrEmpty(sort) ? new SortOption(sort, order) : null;
+            SortOption sorting = new SortOption(sort ?? "", order ?? "");
             OperationResultDto<Tuple<IList<TRes>, long>> result = await Manager.GetAsync(pageNumber, pageSize, sorting, additionalFilters);
             HttpContext.Response.StatusCode = result.Status;
             return new PagedDataDto<TRes>(pageNumber, result.Data.Item2, PagingUtils.GetTotalPages(result.Data.Item2, pageSize), result.Data.Item1);
