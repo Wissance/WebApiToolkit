fa98d9ffde938ded0ab27acf0388a02feb216e37
Ushakov Michale <um.nix.user@gmail.com>
2025-03-23 23:16:34 +0500

    test was done, solved issue with size <= 0

--- Wissance.WebApiToolkit/Wissance.WebApiToolkit.Tests/Controllers/TestBasicReadController.cs
+++ Wissance.WebApiToolkit/Wissance.WebApiToolkit.Tests/Controllers/TestBasicReadController.cs
@@ -12,32 +12,17 @@ namespace Wissance.WebApiToolkit.Tests.Controllers
 {
     public class TestBasicReadController : WebApiTestBasedOnTestApp
     {
-        [Fact]
-        public async Task TestReadAsync()
-        {
-            using(HttpClient client = Application.CreateClient())
-            {
-                HttpResponseMessage resp = await client.GetAsync("/api/Code");
-                Assert.Equal(HttpStatusCode.OK, resp.StatusCode);
-                string pagedDataStr = await resp.Content.ReadAsStringAsync();
-                Assert.True(pagedDataStr.Length > 0);
-                OperationResultDto<PagedDataDto<CodeDto>> result = JsonConvert.DeserializeObject<OperationResultDto<PagedDataDto<CodeDto>>>(pagedDataStr);
-                // TODO(UMV): check very formally only that ReadAsync returns PagedData wrapped in OperationResult
-                Assert.Equal(true, result.Success);
-                // See DataInitializer, just test data by it size that we've received anything
-                Assert.Equal(3, result.Data.Total);
-            }
-        }
-
+        
         [Theory]
-        [InlineData(0, 0, 1, 3)]
-        [InlineData(1, -1, 1, 3)]
-        [InlineData(0, null, 1, 3)]
-        [InlineData(1, null, 1, 3)]
-        [InlineData(-1, -1, 1, 3)]
-        [InlineData(null, null, 1, 3)]
-        [InlineData(1, 10, 1, 3)]
-        public async Task TestReadAsyncWithPagingParamVariation(int? page, int? size, int expectedPage, int expectedTotal)
+        [InlineData(0, 0, 1, 1, 3)]
+        [InlineData(1, -1, 1, 1, 3)]
+        [InlineData(0, null, 1, 1, 3)]
+        [InlineData(1, null, 1, 1, 3)]
+        [InlineData(-1, -1, 1, 1, 3)]
+        [InlineData(null, null, 1, 1, 3)]
+        [InlineData(1, 10, 1, 1, 3)]
+        [InlineData(2, 2, 2, 2, 3)]
+        public async Task TestReadAsync(int? page, int? size, int expectedPage, int expectedPages, int expectedTotal)
         {
             using(HttpClient client = Application.CreateClient())
             {
@@ -61,8 +46,8 @@ namespace Wissance.WebApiToolkit.Tests.Controllers
                 // TODO(UMV): check very formally only that ReadAsync returns PagedData wrapped in OperationResult
                 Assert.NotNull(result);
                 Assert.True(result.Success);
-                // See DataInitializer, just test data by it size that we've received anything
                 Assert.Equal(expectedPage, result.Data.Page);
+                Assert.Equal(expectedPages, result.Data.Pages);
                 Assert.Equal(expectedTotal, result.Data.Total);
             }
         }
@@ -77,8 +62,8 @@ namespace Wissance.WebApiToolkit.Tests.Controllers
                 string readStr = await resp.Content.ReadAsStringAsync();
                 Assert.True(readStr.Length > 0);
                 OperationResultDto<CodeDto> result = JsonConvert.DeserializeObject<OperationResultDto<CodeDto>>(readStr);
-                // TODO(UMV): check very formally only that ReadAsync returns PagedData wrapped in OperationResult
-                Assert.Equal(true, result.Success);
+                Assert.NotNull(result);
+                Assert.True(result.Success);
                 CodeDto expectedCode = new CodeDto()
                 {
                     Id = 2,
