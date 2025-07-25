5af52fb07068ee778fe893bccc1ae4f49bdea3f5
Ushakov Michale <um.nix.user@gmail.com>
2025-07-25 20:29:04 +0500

    1. controller was removed 2. test project on Core was added

---
+++ Wissance.WebApiToolkit/Wissance.WebApiToolkit.Core.Tests/Managers/TestWebFolderFileManager.cs
@@ -0,0 +1,15 @@
+namespace Wissance.WebApiToolkit.Core.Tests.Managers
+{
+    public class TestWebFolderFileManager : IDisposable
+    {
+        public TestWebFolderFileManager()
+        {
+            // prepare a Set of WebFolders
+        }
+
+        public void Dispose()
+        {
+            // remove WebFolders
+        }
+    }
+}
