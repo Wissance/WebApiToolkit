26a9c7988f772a6fbea710fa1dcc38a4252cb25c
Ushakov Michale <um.nix.user@gmail.com>
2025-08-08 23:25:30 +0500

    Create works

--- Wissance.WebApiToolkit/Wissance.WebApiToolkit.Tests/Controllers/TestCrudControllerWithDynamicAutoManager.cs
+++ Wissance.WebApiToolkit/Wissance.WebApiToolkit.Tests/Controllers/TestCrudControllerWithDynamicAutoManager.cs
@@ -1,10 +1,11 @@
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Wissance.WebApiToolkit.Dto;
 using Wissance.WebApiToolkit.TestApp.Data.Entity;
-using Wissance.WebApiToolkit.TestApp.Dto;
 using Wissance.WebApiToolkit.Tests.Utils;
 
 namespace Wissance.WebApiToolkit.Tests.Controllers
@@ -12,7 +13,7 @@ namespace Wissance.WebApiToolkit.Tests.Controllers
     public class TestCrudControllerWithDynamicAutoManager : WebApiTestBasedOnTestApp
     {
         [Fact]
-        public async Task TestReadAsync()
+        public async Task TestReadMany()
         {
             using (HttpClient client = Application.CreateClient())
             {
@@ -26,5 +27,22 @@ namespace Wissance.WebApiToolkit.Tests.Controllers
                 Assert.True(result.Success);
             }
         }
+
+        [Fact]
+        public async Task TestCreate()
+        {
+            using (HttpClient client = Application.CreateClient())
+            {
+                UserEntity newUser = new UserEntity()
+                {
+                    Id = 100,
+                    Login = "ass",
+                    OrganizationId = 4
+                };
+                JsonContent content = JsonContent.Create(newUser);
+                HttpResponseMessage createUserResponse = await client.PostAsync("api/User", content);
+                Assert.Equal(HttpStatusCode.Created, createUserResponse.StatusCode);
+            }
+        }
     }
-}
+} 
