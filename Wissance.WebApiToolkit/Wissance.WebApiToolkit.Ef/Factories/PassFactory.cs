6e06751f4b415e8a2b938631145393a7da5c02a4
Ushakov Michale <um.nix.user@gmail.com>
2025-08-23 14:21:32 +0500

    some modification experiment, rollback is possible

--- Wissance.WebApiToolkit/Wissance.WebApiToolkit.Ef/Factories/PassFactory.cs
+++ Wissance.WebApiToolkit/Wissance.WebApiToolkit.Ef/Factories/PassFactory.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Security.Permissions;
 
@@ -8,7 +10,23 @@ namespace Wissance.WebApiToolkit.Ef.Factories
     {
         public static T Create<T>(T item)
         {
-            return item;
+            Type objType = typeof(T);
+            T res = item;
+            PropertyInfo[] properties = objType.GetProperties();
+            IList<PropertyInfo> virtProperties = properties.Where(p => !p.GetAccessors()[0].IsVirtual && 
+                                                                       !p.PropertyType.IsPrimitive).ToList();
+
+            foreach (PropertyInfo virtProperty in virtProperties)
+            {
+                Type propType = virtProperty.GetType();
+                IList<PropertyInfo> virtTypeProps = propType.GetProperties().Where(p => !p.GetAccessors()[0].IsVirtual && 
+                    !p.PropertyType.IsPrimitive).ToList();
+                foreach (PropertyInfo prop in virtTypeProps)
+                {
+                    prop.SetValue(res, null);
+                }
+            }
+            return res;
         }
 
         // todo(UMV): move Generic on a class level and init properties via static constructor
