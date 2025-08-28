62c210872404a62cba76e059251024cbd6762e90
Ushakov Michale <um.nix.user@gmail.com>
2025-08-28 09:47:05 +0500

    test on pass factory

--- Wissance.WebApiToolkit/Wissance.WebApiToolkit.Ef/Factories/PassFactory.cs
+++ Wissance.WebApiToolkit/Wissance.WebApiToolkit.Ef/Factories/PassFactory.cs
@@ -10,33 +10,47 @@ namespace Wissance.WebApiToolkit.Ef.Factories
     {
         public static T Create<T>(T item)
         {
-            Type objType = typeof(T);
             T res = item;
-            PropertyInfo[] properties = objType.GetProperties();
-            IList<PropertyInfo> virtProperties = properties.Where(p => !p.GetAccessors()[0].IsVirtual && 
-                                                                       !p.PropertyType.IsPrimitive).ToList();
+            PropertyInfo[] properties = item.GetType().GetProperties(BindingFlags.DeclaredOnly | 
+                                                                     BindingFlags.Public | 
+                                                                     BindingFlags.Instance);
+            IList<PropertyInfo> virtProperties = properties.Where(p => p.GetAccessors()[0].IsVirtual && 
+                                                                       !p.PropertyType.IsPrimitive && 
+                                                                       p.PropertyType.IsClass || p.PropertyType.IsInterface).ToList();
 
             foreach (PropertyInfo virtProperty in virtProperties)
             {
-                Type propType = virtProperty.GetType();
-                IList<PropertyInfo> virtTypeProps = propType.GetProperties().Where(p => !p.GetAccessors()[0].IsVirtual && 
-                    !p.PropertyType.IsPrimitive).ToList();
-                foreach (PropertyInfo prop in virtTypeProps)
-                {
-                    prop.SetValue(res, null);
-                }
+                virtProperty.SetValue(res, null);
+                // object propertyValue = virtProperty.GetValue(res);
+                // SetNullForVirtual(propertyValue);
             }
             return res;
         }
 
         // todo(UMV): move Generic on a class level and init properties via static constructor
         public static void UpdateAll<T, TId>(T item, TId id, T updatingItem)
+            where T : class
         {
             Type objType = typeof(T);
             PropertyInfo[] properties = objType.GetProperties();
             foreach (PropertyInfo prop in properties)
             {
                 prop.SetValue(updatingItem, prop.GetValue(item));
+            }
+        }
+
+        private static void SetNullForVirtual<T>(T obj)
+        {
+            PropertyInfo[] properties = obj.GetType().GetProperties(BindingFlags.DeclaredOnly | 
+                                                                     BindingFlags.Public | 
+                                                                     BindingFlags.Instance);
+            IList<PropertyInfo> virtProperties = properties.Where(p => p.GetAccessors()[0].IsVirtual && 
+                !p.PropertyType.IsPrimitive && 
+                p.PropertyType.IsClass || p.PropertyType.IsInterface).ToList();
+
+            foreach (PropertyInfo virtProperty in virtProperties)
+            {
+                virtProperty.SetValue(obj, null);
             }
         }
     }
